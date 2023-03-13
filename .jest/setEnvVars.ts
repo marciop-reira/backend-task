c8af75da176bab8cb981b02d021ce843a82f5452
Márcio Pereira <marciop.usa@gmail.com>
2023-03-13 09:50:10 -0300

    jest setup config, dockerfile mongo and prisma schema

---
+++ .jest/setEnvVars.ts
@@ -0,0 +1,2 @@
+process.env.AVATARS_DIR = "./public/avatars-test/"
+process.env.DATABASE_URL="mongodb://root:prisma@localhost:27017/backend-task-test?authSource=admin"
---
+++ docker/mongo/.bash_history
@@ -0,0 +1,4 @@
+mongosh -u mongo -p mongo
+mongosh -u root -p root
+mongosh -u mongo -p mongo
+mongosh -u mongo -p mongo --authenticationDatabase backend-task
